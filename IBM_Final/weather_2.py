e22c82de2881b8c20e518a5a7195cd54c8755743
KanishR1 <kanishjarvis@gmail.com>
2022-11-12 18:41:08 +0530

    Corrected

--- IBM_Final/weather_2.py
+++ IBM_Final/weather_2.py
@@ -1,13 +1,13 @@
-import requests as req
 import json
 import os
+import random as rd
+import sys
+import time
+from datetime import datetime as dt
+
 import ibmiotf.application
 import ibmiotf.device
-from datetime import datetime as dt
-import time
-import sys
-import random as rd
-
+import requests as req
 
 # Weather Details 
 api_key = "fb12afe2efc8992de27da4156f22f05a"
@@ -40,7 +40,7 @@ auth_token_l = "Jegadees_001"
 default_speed_limit = 60 # km/hrs
 default_horn = True # Horn can be used
 
-hour_now = str(dt.now()).split()[1].split(":")[0]
+hour_now = int(str(dt.now()).split()[1].split(":")[0])
 today = str(dt.now().strftime("%A"))
 
 # Location Information
@@ -50,7 +50,8 @@ location_info = {
         "active_time" : [7,17] # 7 am - 5 pm
         }
         ,
-    "hospitals_near_by" : False, # Randomize Zones
+    "hospitals_near_by" :{
+        'hospital_zone':False}, # Randomize Zones
     "speed_limit" : default_speed_limit,
     "horn" : default_horn
     }
@@ -78,7 +79,7 @@ def myonpublishcallback_s(speed_horn_data):
     print()
     print(f"Speed limit  = { speed_horn_data['speed'] }")
     print(f"Horn Info = {speed_horn_data['Horn']}")
-    print(f"Hospital Zone = {speed_horn_data['Hospital Zone']}")
+    print(f"hospital_zone = {speed_horn_data['hospital_zone']}")
     print(f"School Zone = {speed_horn_data['School Zone']}")
     print(f"Time Now (in hrs) = {speed_horn_data['time_hour']}")
     print()
@@ -86,9 +87,10 @@ def myonpublishcallback_s(speed_horn_data):
 # Speed Limit and Horn Process
 
 def speed_process(climate):
-    climatee=rd.choice(["Rain","Fog","Mist","Smog","Snow"])
-    if climatee == 'Rain':
-        if location_info['hospitals_near_by']:
+    #print(climatee)
+    #print(location_info)
+    if climate == 'Rain':
+        if location_info['hospitals_near_by']['hospital_zone']:
             location_info['horn'] = False
             location_info['speed_limit'] = 15
         elif location_info['school']['school_zone']:
@@ -96,7 +98,7 @@ def speed_process(climate):
                 location_info['horn'] = default_horn
                 location_info['speed_limit'] = 25
             else:
-                if location_info['school']['active_time'][0] >=hour_now  and location_info['school']['active_time'][1]<=hour_now :
+                if location_info['school']['active_time'][0] >= hour_now  and location_info['school']['active_time'][1]<=hour_now :
                     location_info['horn'] = False
                     location_info['speed_limit'] = 15
                 else:
@@ -105,9 +107,9 @@ def speed_process(climate):
         else:
             location_info["horn"] = default_horn
             location_info["speed_limit"] = 20
-    elif climatee == 'Snow' or climate == 'Smog' or climate == 'Fog':
+    elif climate == 'Snow' or climate == 'Smog' or climate == 'Fog':
         location_info['speed_limit'] = 10
-        if location_info['hospitals_near_by']:
+        if location_info['hospitals_near_by']['hospital_zone']:
             location_info['horn'] = False
             
         elif location_info['school']['school_zone']:
@@ -121,7 +123,7 @@ def speed_process(climate):
         else:
             location_info["horn"] = default_horn
     else:
-        if location_info['hospitals_near_by']:
+        if location_info['hospitals_near_by']['hospital_zone']:
             location_info['horn'] = False
             location_info['speed_limit'] = 20
         elif location_info['school']['school_zone']:
@@ -129,7 +131,7 @@ def speed_process(climate):
                 location_info['horn'] = default_horn
                 location_info['speed_limit'] = default_speed_limit
             else:
-                if location_info['school']['active_time'][0] >=hour_now  and location_info['school']['active_time'][1]<=hour_now :
+                if (location_info['school']['active_time'][0] >=hour_now ) and (location_info['school']['active_time'][1]<=hour_now) :
                     location_info['horn'] = False
                     location_info['speed_limit'] = 20
                 else:
@@ -172,11 +174,18 @@ deviceCli_s.connect()
 
 # Processing
 while True:
+    location_info['hospitals_near_by']['hospital_zone']=rd.choice([True,False])
+    location_info['school']['school_zone']=rd.choice([True,False])
+    climatee=rd.choice(["Rain","Fog","Mist","Smog","Snow"])
+    print(climatee)
     data = get_weather_details()
-    speed_process(data['climate'])
+    #speed_process(data['climate'])
+    speed_process(climatee)
     success_w = deviceCli_w.publishEvent("Current Weather","json",data,qos=1,on_publish = myonpublishcallback_w(data))
     horn_data = "Usage of Horn Allowed" if location_info['horn'] else "Do not use the horn frequently"
-    speed_horn_data = {"Hospital Zone":location_info['hospitals_near_by'],"School Zone":location_info['school']['school_zone'],"time_hour":hour_now,"speed":location_info['speed_limit'],"Horn":horn_data}
+    speed_horn_data = {"hospital_zone":location_info['hospitals_near_by']['hospital_zone'],"School Zone":location_info['school']['school_zone'],"time_hour":hour_now,"speed":location_info['speed_limit'],"Horn":horn_data}
     success_s = deviceCli_s.publishEvent("Speed Limit","json",speed_horn_data,qos=1,on_publish = myonpublishcallback_s(speed_horn_data))
     if not success_w and not success_s:
         time.sleep(1)
+    time.sleep(3)
+
